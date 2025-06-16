dc7919813f611596c03a5f87fcb6855ef234d0b3
Gabr1elFraga <82734492+Gabr1elFraga@users.noreply.github.com>
2025-06-15 23:21:09 -0300

    Update lexico.py

--- src/lexico.py
+++ src/lexico.py
@@ -1,1 +1,54 @@
+#import ply.lex as lex
+
+reservadas = {
+    'abstract' : 'ABSTRACT',
+    'alias' : 'ALIAS',
+    'as' : 'AS',
+    'asm' : 'ASM',
+    'begin' : 'BEGIN',
+    'break' : 'BREAK',
+    'case' : 'CASE',
+    'class' : 'CLASS',
+    'def' : 'DEF',
+    'do' : 'DO',
+    'else' : 'ELSE',
+    'elsif' : 'ELSIF',
+    'end' : 'END',
+    'ensure' : 'ENSURE',
+    'enum' : 'ENUM',
+    'extends' : 'EXTENDS',
+    'false' : 'FALSE',
+    'for' : 'FOR',
+    'if' : 'IF',
+    'in' : 'IN',
+    'include' : 'INCLUDE',
+    'is_a?' : 'IS_A_QUERY',
+    'macro' : 'MACRO',
+    'module' : 'MODULE',
+    'next' : 'NEXT',
+    'nil' : 'NIL',
+    'of' : 'OF',
+    'private' : 'PRIVATE',
+    'protected' : 'PROTECTED',
+    'require' : 'REQUIRE',
+    'rescue' : 'RESCUE',
+    'return' : 'RETURN',
+    'self' : 'SELF',
+}
+#Tokens 
+
+tokens = (
+    ''
+)
+
+
+    
+
+
+
+
+
+
+
+
 
