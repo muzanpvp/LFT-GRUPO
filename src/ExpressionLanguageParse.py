65263403fa076ae06b0f39112e9192eb5e0fafb2
Gabr1elFraga <gabrielsilveirafraga@hotmail.com>
2025-08-13 09:00:46 -0300

    Criando sintatico

--- src/ExpressionLanguageParse.py
+++ src/ExpressionLanguageParse.py
@@ -2,3 +2,24 @@ import ply.yacc as yacc
 import ExpressionLanguageLex as lex 
 reservadas = lex.reservadas
 
+def p_program(p):
+    '''program : opt_require opt_globals opt_classes code_functions'''
+
+def p_opt_require(p):
+    '''opt_require  :   require
+                    |   empty'''
+
+def p_opt_globals(p):
+    '''opt_globals  :   global_declarations
+                    |   empty'''
+
+def p_opt_classes(p):
+    '''opt_classes  :   class_functions
+                    |    empty'''
+
+def p_empty(p):
+    'empty :'
+    pass
+
+def p_require(p):
+    'require  : REQUIRE ID delimitador'
