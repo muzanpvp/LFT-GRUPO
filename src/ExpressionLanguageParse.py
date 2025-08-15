4a74a26e958b27ef1c97f5eccc27d096c88ff651
Gabr1elFraga <gabrielsilveirafraga@hotmail.com>
2025-08-15 08:27:41 -0300

    Modificando Sintatico

--- src/ExpressionLanguageParse.py
+++ src/ExpressionLanguageParse.py
@@ -1,6 +1,7 @@
+import ply.lex as lex
 import ply.yacc as yacc
-import ExpressionLanguageLex as lex 
-reservadas = lex.reservadas
+from ExpressionLanguageLex import *
+
 
 def p_program(p):
     '''program : opt_require opt_globals opt_classes code_functions'''
@@ -22,4 +23,33 @@ def p_empty(p):
     pass
 
 def p_require(p):
-    'require  : REQUIRE ID delimitador'
+    'require    :   REQUIRE ID delimitador'
+
+def p_globals(p):
+    'globals    :   AT ID'
+
+
+def p_types(p):
+    '''types    :   intenger
+                |   float 
+                |   boolean'''
+    
+def p_integer(p):
+    '''interger :   INT
+                |   INT8
+                |   INT16
+                |   INT32
+                |   INT64
+                |   INT128'''
+    
+def p_float(p):
+    '''float    :   FLOAT
+                |   FLOAT32
+                |   FLOAT64'''
+    
+def p_boolean(p):
+    'boolean    :   BOOL'
+
+
+
+
